d6c7169553d535039579fabea72001258b952c1f
Pinder47 <wootimus.prime@gmail.com>
2017-03-17 09:50:36 -0700

    message

--- exercises.js
+++ exercises.js
@@ -109,12 +109,6 @@ function cacheFunction(cb) {
 	//if the function you return is invoked with 5 it would pass 5 to cb(5) and return 25
 	//if the function you return is invoked again with 5 it will look on an object in the closure scope
 	//and return 25 directly and will not invoke cb again
-/*	return function (arg) {
-		var start = arg;
-		var result = cb(start);
-		return result;
-	};*/
-
 	var argCache = {};
 	return function(arg){
 		if (!argCache.hasOwnProperty(arg)){
